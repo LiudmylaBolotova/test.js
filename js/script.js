e7f423c0eefd7c225e8b4b1b9639911c6bb042eb
LiudmylaBolotova <89790016+LiudmylaBolotova@users.noreply.github.com>
2021-12-11 21:41:51 +0200

    добавляет примеры

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// let base = prompt('go namber');
+// console.log(base = (Number(base)));
+
+// let power = prompt('go stepen');
+// power = Number(power);
+
+// console.log(power);
+
+// const result = base ** power;
+// console.log(result);
+
+
+
+// const max = 50;
+// const min = 30;
+
+// const result = Math.random() * (max - min) + min;
+// console.log(Math.round(result));
+
+
+// const colors = ['tomato', 'green', 'blue', 'pink', 'gray', 'teal'];
+// const max = colors.length - 1;
+// const min = 0;
+// const index = Math.round(Math.random() * (max - min) + min);
+// const color = colors[index];
+
+// console.log(color);
+
+// document.body.style.backgroundColor = color;
+
+
+
+// let brand = 'SamsUng';
+// brand = brand.toUpperCase();
+// console.log(brand);
+
+// let brand = 'SamsUng';
+// brand = brand.toLowerCase();
+// console.log(brand);
+
+
+// const blackListedWord1 = 'спам';
+// const blackListedWord2 = 'распродажа';
+// const blackListedWord3 = 'спешите';
+
+// const srting1 = 'Только сегодня на распродаже вы можете купить деньги';
+// const srting2 = 'Это не спам, спешите приобрести мозги';
+// const srting3 = 'Приходите к нам на ярмарку';
+
+// console.log(srting1.includes(blackListedWord1));
+// console.log(srting1.includes(blackListedWord2));
+// console.log(srting1.includes(blackListedWord3));
+
+// console.log(srting2.includes(blackListedWord1));
+// console.log(srting2.includes(blackListedWord2));
+// console.log(srting2.includes(blackListedWord3));
+
+// console.log(srting3.includes(blackListedWord1));
+// console.log(srting3.includes(blackListedWord2));
+// console.log(srting3.includes(blackListedWord3));
