8de09dafeff9ea165ba84d672c7cbbc69d17f0ae
LiudmylaBolotova <89790016+LiudmylaBolotova@users.noreply.github.com>
2022-05-28 16:19:25 +0300

    test

--- js/script5.js
+++ js/script5.js
@@ -1,18 +1,42 @@
-function checkForSpam(message) {
-  let result = " ";
+
+function calculateTotalPrice (orderedQuantity, pricePerItem) {
   // Change code below this line
-    if (message.includes("sale")) {
-        return result = true;
-        
-    }
+  const totalPrice = orderedQuantity * pricePerItem;
 
-    else {
-        return result = false;
-    }
-     console.log(result);
   // Change code above this line
-
+  return totalPrice;
 };
 
-checkForSpam("Latest technology news");
-checkForSpam('Get best sale offers now!');
+calculateTotalPrice(10, 5);
+
+console.log(calculateTotalPrice(10, 5));
+
+
+function getDiscount(totalSpent) {
+    const BASE_DISCOUNT = 0;
+    const BRONZE_DISCOUNT = 0.02;
+    const SILVER_DISCOUNT = 0.05;
+    const GOLD_DISCOUNT = 0.1;
+    let discount;
+    // Change code below this line
+    if (totalSpent >= 50000) {
+       
+        discount = 0,1;
+    } else if (totalSpent >= 20000 && totalSpent < 50000) {
+        
+        discount = 0,05;
+    } else if (totalSpent >= 5000 && totalSpent < 20000) {
+       
+        discount = 0,02;
+    } else if (totalSpent < 500) {
+        
+        discount = 0;
+    }
+    // Change code above this line
+    return discount;
+}
+
+
+console.log(discount);
+getDiscount(137000);
+getDiscount(46900);
