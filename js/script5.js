303bec1f5e92bd94aaf0c79cdd07a35577284f39
LiudmylaBolotova <89790016+LiudmylaBolotova@users.noreply.github.com>
2022-05-29 12:21:20 +0300

    test

--- js/script5.js
+++ js/script5.js
@@ -1,42 +1,8 @@
-
-function calculateTotalPrice (orderedQuantity, pricePerItem) {
-  // Change code below this line
-  const totalPrice = orderedQuantity * pricePerItem;
-
-  // Change code above this line
-  return totalPrice;
-};
-
-calculateTotalPrice(10, 5);
-
-console.log(calculateTotalPrice(10, 5));
-
-
-function getDiscount(totalSpent) {
-    const BASE_DISCOUNT = 0;
-    const BRONZE_DISCOUNT = 0.02;
-    const SILVER_DISCOUNT = 0.05;
-    const GOLD_DISCOUNT = 0.1;
-    let discount;
+function calculateEngravingPrice(message, pricePerWord) {
     // Change code below this line
-    if (totalSpent >= 50000) {
-       
-        discount = 0,1;
-    } else if (totalSpent >= 20000 && totalSpent < 50000) {
-        
-        discount = 0,05;
-    } else if (totalSpent >= 5000 && totalSpent < 20000) {
-       
-        discount = 0,02;
-    } else if (totalSpent < 500) {
-        
-        discount = 0;
-    }
+   message.split(' ');
+    return message;
     // Change code above this line
-    return discount;
 }
-
-
-console.log(discount);
-getDiscount(137000);
-getDiscount(46900);
+console.log(message);
+calculateEngravingPrice("My really switty", 80);
