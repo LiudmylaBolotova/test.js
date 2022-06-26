6f28d578b4e2c190541172bec61d581509b20f5a
LiudmylaBolotova <89790016+LiudmylaBolotova@users.noreply.github.com>
2022-06-26 16:59:11 +0300

    update

--- js/script5.js
+++
@@ -1,8 +0,0 @@
-function calculateEngravingPrice(message, pricePerWord) {
-    // Change code below this line
-   message.split(' ');
-    return message;
-    // Change code above this line
-}
-console.log(message);
-calculateEngravingPrice("My really switty", 80);
